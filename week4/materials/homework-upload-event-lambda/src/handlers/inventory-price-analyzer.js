63df5075d1f8010a5208d241041b21185b550362
keerthi <keerthi1822@gmail.com>
2021-10-03 13:33:50 +0200

    week4 in progress

--- week4/materials/homework-upload-event-lambda/src/handlers/inventory-price-analyzer.js
+++ week4/materials/homework-upload-event-lambda/src/handlers/inventory-price-analyzer.js
@@ -1,25 +1,42 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
 exports.s3PriceAnalyzer = async (event) => {
-  const getObjectRequests = event.Records.map(record => {
+  const getObjectRequests = event.Records.map((record) => {
     const params = {
       Bucket: record.s3.bucket.name,
-      Key: record.s3.object.key
+      Key: record.s3.object.key,
     };
 
-    return s3.getObject(params).promise().then(data => {
-      // implement code here
-      let items = data.Body.toString().split("\n")
-      items.forEach(item => console.log(item))
-
-    }).catch(err => {
-      console.error("Error calling S3 getObject:", err);
-      return Promise.reject(err);
-    })
+    return s3
+      .getObject(params)
+      .promise()
+      .then((data) => {
+        // implement code here
+        let items = data.Body.toString().split("\n");
+        const SplitItems = items.map((item) => item.split(","));
+        const filteredItems = SplitItems.filter((itemToFilter) => {
+          return itemToFilter[0] === "fruits" && itemToFilter[2] < 10;
+        });
+        console.log(filteredItems);
+      })
+      .then((filteredItems) => {
+        const respones = filteredItems
+          .map((itemArray) => itemArray)
+          .map((item) =>
+            fetch(
+              `https://hooks.slack.com/services/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`,
+              { text: "Hello, World! " + item }
+            ).then((res) => res.json())
+          );
+      })
+      .catch((err) => {
+        console.error("Error calling S3 getObject:", err);
+        return Promise.reject(err);
+      });
   });
 
-  return Promise.all(getObjectRequests).then(() => {
-    console.debug('Complete!');
+  return Promise.all(respones).then(() => {
+    console.debug("Complete!");
   });
-}
+};
